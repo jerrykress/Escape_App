5938fd7e965f7b7181897053e0e3069a54f1ec21
Jerry Kress <jerrykress.wsj@gmail.com>
2020-06-02 12:49:38 +0100

    Initial Commit

---
+++ Velvet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Velvet
+//
+//  Created by Jerry Kress on 02/06/2020.
+//  Copyright © 2020 Jerry Kress. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
