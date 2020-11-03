aba074d617263d8ccc370b95c2f9980a010e94f6
Jerry Kress <jerrykress.wsj@gmail.com>
2020-11-03 23:57:32 +0000

    Timer Negative Bug Fix

--- Escape/TimerHandler.swift
+++ Escape/TimerHandler.swift
@@ -38,7 +38,7 @@ struct TimerHandler: View {
                     .foregroundColor(Color.white)
                     .frame(width: 20, height: 20, alignment: .center)
                 
-                Text(self.userData.timer == 0 ? "Off" : "\(self.userData.timer)")
+                Text(self.userData.timer < 1 ? "Off" : "\(self.userData.timer)")
                                 .foregroundColor(Color.white)
                                 .font(.custom("DIN Alternate", size: 13))
                                 .opacity(0.5)
